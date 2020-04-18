63682525fa197eaa3829295ffb74035149dbaa29
Ramon de Lima Ramos <ramonlimaramos@gmail.com>
2020-04-18 11:45:06 -0300

    feat: emit signal when developer add
    Signed-off-by: Ramon de Lima Ramos <ramonlimaramos@gmail.com>

--- backend/src/websocket.js
+++ backend/src/websocket.js
@@ -1,11 +1,41 @@
 const socketio = require("socket.io");
+const parseStringAsArray = require("./utils/parseStringAsArray");
+const calculateDistanceFromLatLongInKm = require("./utils/calculateDistanceFromLatLongInKm");
+
+let io;
+const connections = []; //TODO - change me to redis db;
 
 exports.setupWebSocket = (server) => {
 
-    const io = socketio(server);
+    io = socketio(server);
 
     io.on("connection", (socket) => {
         
-        console.log(socket.id);
+        const {latitude, longitude, techs} = socket.handshake.query;
+
+        connections.push({
+            id: socket.id,
+            coordinates: {
+                latitude: Number(latitude),
+                longitude: Number(longitude),
+            },
+            techs: parseStringAsArray(techs)
+        })
     })
+}
+
+exports.findConnections = (coordinates, techs) => {
+    
+    // Check it out if the coordinates of socket connections are less then 10 km and exist at least one techs
+    return connections.filter(connection => {
+        return calculateDistanceFromLatLongInKm(coordinates, connection.coordinates) < 10
+            && connection.techs.some(techsItem => techs.includes(techsItem));
+    })
+};
+
+
+exports.sendMessage = (to, message, data) => {
+    to.forEach(connection => {
+        io.to(connection.id).emit(message, data);
+    });
 }
